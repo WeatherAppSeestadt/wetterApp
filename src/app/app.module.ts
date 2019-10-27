bbae287a8dd0577f33c05405a63674c4c25d7367
Verenasworld <verena.felizidas@gmail.com>
2019-10-27 20:57:03 +0100

    first session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.moduls';
+import {AppComponent} from './app.component';
+
+import {HomeComponent} from './pages/home/home.component';
+import {DetailsComponent} from './pages/details/details.component';
+import {HttpClientModule} from '@angular/common/http';
+import {WeatherCardComponent} from './ui/weather-card/weather-card.component';
+import {AddCardComponent} from './ui/add-card/add-card.component';
+import {AddComponent} from './pages/add/add.component';
+import {LoginComponent} from './pages/login/login.component';
+import {SignupComponent} from './pages/signup/signup.component';
+import {ErrorComponent} from './ui/error/error.component';
+
+import {environment} from '../environments/environment';
+
+import {FormsModule} from '@angular/forms';
+import {AngularFireLite} from 'angularfire-lite';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    AddComponent,
+    LoginComponent,
+    SignupComponent,
+    ErrorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireLite.forRoot(environment.config),
+
+    FormsModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
